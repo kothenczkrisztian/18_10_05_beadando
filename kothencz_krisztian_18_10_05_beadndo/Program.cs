566075d3eb953c4b07fd3fea3971ce2b6f34ecba
Petrik Lajos Szg <info@petrik.hu>
2018-10-26 19:18:53 +0200

    feladat_1.0

--- kothencz_krisztian_18_10_05_beadndo/Program.cs
+++ kothencz_krisztian_18_10_05_beadndo/Program.cs
@@ -10,6 +10,32 @@ namespace kothencz_krisztian_18_10_05_beadndo
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Kérem ajon meg 10 számot:  ");
+            int[] szamok = new int[10];
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                Console.Write((i + 1) + ". ");
+                szamok[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine();
+            Console.WriteLine("A 10 bekért szám :");
+
+            foreach (var item in szamok)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            int max = 0;
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                int akt_ertek = szamok[i];
+                if (akt_ertek > max)
+                {
+                    max = akt_ertek;
+                }
+            }
+            Console.WriteLine("A legnagyobb szám : " + max);
+            Console.ReadKey();
         }
     }
 }
